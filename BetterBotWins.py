e12ac4180ce8215073e2cd8b64457c6f91e6b7eb
DARKSONIC <kevin-lehmann99@web.de>
2022-10-05 12:24:34 +0200

    Solved guild/server Problem, add dm greetings

--- BetterBotWins.py
+++ BetterBotWins.py
@@ -4,15 +4,26 @@ from dotenv import load_dotenv
 
 load_dotenv()
 TOKEN = os.getenv('DISCORD_TOKEN')
+GUILD = os.getenv('DISCORD_SERVER')
 
 client = discord.Client(intents=discord.Intents.default())
 
 @client.event
 async def on_ready():
-    print(f'{client.user} has connected to Discord')
+    guild = discord.utils.get(client.guilds, name=GUILD)
+    print(
+        f'{client.user} is connected to the following guild:\n'
+        f'{guild.name}(id: {guild.id})'
+    )
+
+    members = '\n - '.join([member.name for member in guild.members])
+    print(f'Server Members:\n - {members}')
+
+@client.event
+async def on_member_join(member):
+    await member.create_dm()
+    await member.dm_channel.send(
+        f'Hi {member.name}, welcome to my realm!'
+    )
 
 client.run(TOKEN)
-# Das ist ein Test 
-#Working
-#VSCode Test
-#Branch Test vollendet
